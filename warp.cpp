f8c7c1ea455084e0770463d23ec1feb60508d74e
Eugene Scherba <escherba@livefyre.com>
2014-06-05 22:53:14 -0700

    keep width fixed

--- warp.cpp
+++ warp.cpp
@@ -111,19 +111,21 @@ void project_cylinder(cv::Mat& dst, cv::Mat& src)
     // precompute some constants
     const float half_width = (float)width * 0.50f;
     const float zfr = cos(half_width / r); // distance from cylinder center to projection plane
+    const float x_scale = r * sin(half_width / r) / half_width;
 
     for (int i = 0; i < width; i++)
     {
         // X-axis (columns)
+        //
         const float zb = r * (cos((half_width - (float)i) / r) - zfr);
         const float z_ratio = (zb + zf) / zf;
 
-        const float i_src = r * asin(((float)i - xf) * z_ratio / r) + half_width;
+        const float i_src = r * asin(x_scale * ((float)i - xf) * z_ratio / r) + half_width;
         //const float i_src = xf + z_ratio * r * sin(((float)i - half_width) / r);
         for (int j = 0; j < height; j++)
         {
             // Y-axis (rows)
-            const float j_src = yf + z_ratio * ((float)j - yf);
+            const float j_src = yf + z_ratio * x_scale * ((float)j - yf);
             if (i_src >= 0.0f && i_src <= (float)width &&
                 j_src >= 0.0f && j_src <= (float)height)
             {
@@ -133,18 +135,62 @@ void project_cylinder(cv::Mat& dst, cv::Mat& src)
     }
 }
 
+cv::Mat* project_cylinder2(cv::Mat& src)
+{
+    // Project from a cylindrical background onto a flat screen
+    //
+    const int height = src.rows;
+    const int width  = src.cols;
+
+    const float r  = (float)width * 0.5f;    // cylinder radius ((width / 2) <= r < inf)
+    const float zf = hypot((float)height, (float)width);
+                                             // distance from camera to screen (default: hypothenuse)
+    const float xf = (float)width * 0.5f;     // distance from camera to left edge
+    const float yf = (float)height * 0.75f;  // distance from camera to ceiling
+
+    // precompute some constants
+    const float half_width = (float)width * 0.5f;
+    const float zfr = cos(half_width / r); // distance from cylinder center to projection plane
+    const float xfr = sin(half_width / r);
+    const float x_scale = r * xfr / half_width;
+
+    const int adj_height = (int)ceil((float)height / x_scale);
+
+    std::cout << "before height: " << height << ", after height: " << adj_height << std::endl;
+    cv::Mat *dst = new cv::Mat(cv::Mat::zeros(adj_height, width, src.type()));
+
+    for (int i = 0; i < width; i++)
+    {
+        // X-axis (columns)
+        //
+        const float zb = r * (cos((half_width - (float)i) / r) - zfr);
+        const float z_ratio = (zb + zf) / zf;
+
+        const float i_src = r * asin(x_scale * ((float)i - xf) * z_ratio / r) + half_width;
+        //const float i_src = xf + z_ratio * r * sin(((float)i - half_width) / r);
+        for (int j = 0; j < adj_height; j++)
+        {
+            // Y-axis (rows)
+            const float j_src = yf + z_ratio * (x_scale * (float)j - yf);
+            if (i_src >= 0.0f && i_src <= (float)width &&
+                j_src >= 0.0f && j_src <= (float)height)
+            {
+                dst->at<cv::Vec4b>(j, i) = getSubpix(src, cv::Point2f(i_src, j_src));
+            }
+        }
+    }
+    return dst;
+}
+
 int main(int argc, char *argv[]) {
     cv::Mat src = cv::imread(argv[1], CV_LOAD_IMAGE_UNCHANGED);
     cv::cvtColor(src, src, CV_RGB2RGBA);
     src.convertTo(src, CV_8UC4); // convert to 8-bit BGRA
 
-    // fix jaggies on the bottom by adding a 2-px black border
-    cv::copyMakeBorder(src, src, 2, 2, 0, 0, cv::BORDER_CONSTANT, cv::Scalar(0, 0, 0, 0));
+    // fix jaggies on top and bottom by adding a 2-px border
+    cv::copyMakeBorder(src, src, 2, 2, 0, 0, cv::BORDER_CONSTANT, cv::Scalar::all(0));
 
-    // prepare black destination canvas
-    cv::Mat dst = cv::Mat::zeros(src.rows, src.cols, CV_8UC4);
-
-    project_cylinder(dst, src);
-    cv::imwrite(argv[2], dst);
+    cv::Mat *dst = project_cylinder2(src);
+    cv::imwrite(argv[2], *dst);
 }
 
